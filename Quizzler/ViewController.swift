f8e6458bc3228eade9fc00693273aaed3baf1334
José Pino <joselopino@gmail.com>
2017-12-27 17:25:50 -0300

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Angela Yu on 25/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Place your instance variables here
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    // Cuando el usuario presiona un boton. Se diferencia por Tags, True = 1, False = 2
+    @IBAction func answerPressed(_ sender: AnyObject) {
+        
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
