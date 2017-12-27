072f83b3ba4c0aba22388774af14b8e0e5ef88a4
José Pino <joselopino@gmail.com>
2017-12-27 18:14:47 -0300

    Mostramos la primera pregunta al usuario.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,12 +21,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
     }
 
     
     // Cuando el usuario presiona un boton. Se diferencia por Tags, True = 1, False = 2
     @IBAction func answerPressed(_ sender: AnyObject) {
-        
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
     }
     
     
